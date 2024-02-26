68cf0608527a6a7ea139a0256b86c98779b8a159
sherrerah3 <140777579+sherrerah3@users.noreply.github.com>
2024-02-26 11:41:24 -0500

    Add files via upload

--- ObjetosDePersona.java
+++ ObjetosDePersona.java
@@ -1,7 +1,30 @@
 public class ObjetosDePersona {
     public static void main(String[] args) {
-        //Herencia es la capacidad de crear clases y objetos en base a otras clases
-        Persona Estudiante = new Persona("Samuel", "Herrera", 18);
-        System.out.println("Nombre: " + Estudiante.getNombre() );
+
+        //La herencia es la capacidad de crear clases y objetos en base a otras clases
+        Persona Estudiante = new Persona("Samuel", "Herrera", 17, 1017926398, "EAFIT");
+        System.out.println("\nDatos sobre el Estudiante");
+        Estudiante.saludar();
+        Estudiante.mostrarInfo();
+        System.out.println("Es mayor de edad? " + Estudiante.MayorDeEdad());
+
+
+        Persona Docente = new Persona("Mariana", "Restrepo", 33, 43502246);
+        System.out.println("\nDatos sobre el Docente");
+        Docente.saludar();
+        Docente.mostrarInfo();
+        System.out.println("Es mayor de edad? " + Docente.MayorDeEdad());
+
+
+        Persona Administrativo = new Persona("Carlos", "Sepulveda", 63, 32535046);
+        System.out.println("\nDatos sobre el Administrativo");
+        Administrativo.saludar();
+        Administrativo.mostrarInfo();
+        System.out.println("Es mayor de edad? " + Administrativo.MayorDeEdad());
+
+
+
+        
     }
+
 }
--- Persona.java
+++ Persona.java
@@ -1,18 +1,87 @@
-// Abstracción (capacidad de crear una clase con propiedades y métodos)
+    // Abstracción (capacidad de crear una clase con propiedades y métodos)
 public class Persona {
 
     // Encapsulamiento (Por hacer uso de private y static )
-    private static String nombre, apellido;
-    private static int edad;
+    private static String nombre, apellido, universidad;
+    private static int edad, documento;
 
     // Constructor
     // Polimorfismo (Es que un mismo método ejecute diferentes tareas)
 
-    public Persona(String nombre, String apellido, int edad){
+    public Persona(String nombre, String apellido, int edad, int documento, String universidad){
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
+        this.documento = documento;
+        this.universidad = universidad; 
     }
 
+    public Persona(String nombre, String apellido, int edad, int documento){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.documento = documento;
+        this.universidad = "UDEA";
+    }
+
+
+
+
+    public static String getNombre() {
+        return nombre;
+    }
+
+    public static void setNombre(String Nombre) {
+        Persona.nombre = nombre;
+    }
+
+    public static String getApellido() {
+        return apellido;
+    }
+
+    public static void setApellido(String apellido) {
+        Persona.apellido = apellido;
+    }
+
+    public static int getEdad() {
+        return edad;
+    }
+
+    public static void setEdad(int edad) {
+        Persona.edad = edad;
+    }
+
+    public static int getDocumento(){
+        return documento;
+    }
+
+    public static void setDocumento(int documento){
+        Persona.documento = documento;
+    }
+
+    public static String getUniversidad(){
+        return Persona.universidad;
+    }
+
+    public static void setUniversidad(String universidad){
+        Persona.universidad = universidad;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Edad: " + edad);
+        System.out.println("Documento: " + documento);
+        System.out.println("Universidad: " + universidad);
+    }
+
+    public boolean MayorDeEdad() {
+        int edadLegal = 18;
+        return this.edad >= edadLegal;
+    }
     
+    public void saludar() {
+        System.out.println("¡Hola, soy " + this.nombre + "!");
+    }
+    
+
 }
